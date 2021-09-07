65d7880fe6fb1e004f65e11678ce411c7d75b4b3
Aldair Avalos <aldair.avalos.2000@gmail.com>
2021-09-07 12:15:05 -0500

    project data filtering

--- lists_and_dicts.py
+++ lists_and_dicts.py
@@ -1,3 +1,79 @@
+# NEEDS union/pipe operator (|) available from python >= 3.9
+# https://www.python.org/dev/peps/pep-0584/#specification
+from functools import reduce
+
+DATA = [
+    {
+        'name': 'Facundo',
+        'age': 72,
+        'organization': 'Platzi',
+        'position': 'Technical Coach',
+        'language': 'python',
+    },
+    {
+        'name': 'Luisana',
+        'age': 33,
+        'organization': 'Globant',
+        'position': 'UX Designer',
+        'language': 'javascript',
+    },
+    {
+        'name': 'Héctor',
+        'age': 19,
+        'organization': 'Platzi',
+        'position': 'Associate',
+        'language': 'ruby',
+    },
+    {
+        'name': 'Gabriel',
+        'age': 20,
+        'organization': 'Platzi',
+        'position': 'Associate',
+        'language': 'javascript',
+    },
+    {
+        'name': 'Isabella',
+        'age': 30,
+        'organization': 'Platzi',
+        'position': 'QA Manager',
+        'language': 'java',
+    },
+    {
+        'name': 'Karo',
+        'age': 23,
+        'organization': 'Everis',
+        'position': 'Backend Developer',
+        'language': 'python',
+    },
+    {
+        'name': 'Ariel',
+        'age': 32,
+        'organization': 'Rappi',
+        'position': 'Support',
+        'language': '',
+    },
+    {
+        'name': 'Juan',
+        'age': 17,
+        'organization': '',
+        'position': 'Student',
+        'language': 'go',
+    },
+    {
+        'name': 'Pablo',
+        'age': 32,
+        'organization': 'Master',
+        'position': 'Human Resources Manager',
+        'language': 'python',
+    },
+    {
+        'name': 'Lorena',
+        'age': 56,
+        'organization': 'Python Organization',
+        'position': 'Language Maker',
+        'language': 'python',
+    },
+]
 
 def separator(sep='-'):
     print(sep*40)
@@ -44,6 +120,29 @@ def run():
     sqrt = {num: round(num**0.5, 2) for num in range(1,1000)}
     print(sqrt)
     separator()
+    nums = [1,2,3,4,5]
+    sq = list(map(lambda num: num**2, nums ))
+    print(sq)
+    separator()
+    power = reduce(lambda num, next: num*next, [2,2,2,2,2])
+    print(power)
+
+def project_filter():
+    # Una con list comprehension en otra con high order functions
+    separator('5')
+    pythoners1 = list(filter(lambda person: person['language'] == 'python', DATA))
+    pythoners2 = [person['name'] for person in DATA if person['language'] == 'python']
+    separator()
+    all_Platzi_workers1 = [person['name'] for person in DATA if person['organization'] == 'Platzi']
+    all_Platzi_workers2 = list(map(lambda worker: worker['name'],list(filter(lambda worker: worker['organization'] == 'Platzi', DATA))))
+    separator()
+    adults = list(filter(lambda person: person['age'] > 17, DATA))
+    adult_names1 = list(map(lambda adult: adult['name'],adults))
+    adult_names2 = [worker['name'] for worker in DATA if worker['age'] > 17]
+    separator()
+    old_people1 = list(map(lambda worker: worker | {'old': worker['age'] > 70}, DATA))
+    old_people2 = [(worker | {'old': worker['age'] > 70}) for worker in DATA]
     
 if __name__ == '__main__':
     run();
+    project_filter();
