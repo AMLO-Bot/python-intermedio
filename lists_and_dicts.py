51c7e282eb61ae200312a9b29ddfe7ca6a2001f1
Aldair Avalos <aldair.avalos.2000@gmail.com>
2021-09-07 02:10:54 -0500

    class 10 end of list/dict comprehension section

--- lists_and_dicts.py
+++ lists_and_dicts.py
@@ -1,7 +1,13 @@
+
+def separator(sep='-'):
+    print(sep*40)
+
 def run():
+    # Repaso
     my_list = [1,'hello', True, 4.5]
     my_dict = {'firstname': 'Aldair', 'lastname': 'Avalos'}
 
+    # Lista de dictionarios y dictionarios de listas
     super_list = [
         {'firstname': 'Aldair', 'lastname': 'Avalos'},
         {'firstname': 'Fernanda', 'lastname': 'Albino'},
@@ -17,9 +23,27 @@ def run():
 
     for key, value in super_dict.items():
         print(key, '-', value)
-    print('-'*40)
+    separator()
     for student in super_list:
         print(student['firstname'], '-', student['lastname'])
+    separator()
 
+
+    # Lista con los 10 primeros numeros naturales al cuadrado
+    squared = [num**2  for num in range(1,101) if num%3 != 0]
+    print (squared)
+    separator()
+    multiples = [num for num in range(1,10) if num%36 == 0]
+    print(multiples)
+    separator()
+
+    # Dictionary comprehension
+    cubed = {num: num**3 for num in range(1,101) if num%3 != 0}
+    print(cubed)
+    separator()
+    sqrt = {num: round(num**0.5, 2) for num in range(1,1000)}
+    print(sqrt)
+    separator()
+    
 if __name__ == '__main__':
     run();
